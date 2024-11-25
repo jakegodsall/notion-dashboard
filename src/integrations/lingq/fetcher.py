c21dada3a16094faf19e09903b29a29d44d4a315
Jake Godsall <jake.edward.godsall@gmail.com>
2024-11-25 18:42:17 +0000

    added shell of LingQFetcher component

---
+++ src/integrations/lingq/fetcher.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class LingQFetcher:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "https://www.lingq.com/api/v2/"
+
+    def fetch_languages(self, active = True):
+        url = self.base_url + "languages"
+        headers = {
+            'Authorization': 'Token ' + self.api_key,
+            'accept': 'application/json'
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data
+
+
+if __name__ == '__main__':
+    fetcher = LingQFetcher("MY KEY")
+    languages = fetcher.fetch_languages()
+    print(languages[0])
