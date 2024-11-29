674b953331e47c8f049455bf0d32583159bce4bb
Jake Godsall <jake.edward.godsall@gmail.com>
2024-11-29 08:27:15 +0000

    added method for getting daily word counts

--- src/integrations/lingq/fetcher.py
+++ src/integrations/lingq/fetcher.py
@@ -1,4 +1,9 @@
+import os
+from datetime import datetime
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class LingQFetcher:
@@ -6,21 +11,33 @@ class LingQFetcher:
         self.api_key = os.getenv("LINGQ_API_KEY")
         self.base_url = "https://www.lingq.com/api/v2/"
 
-    def fetch_languages(self, active = True):
-        url = self.base_url + "languages"
+    def make_api_request(self, endpoint):
+        url = self.base_url + endpoint
         headers = {
             'Authorization': 'Token ' + self.api_key,
             'accept': 'application/json'
         }
 
-        response = requests.get(url, headers=headers)
+        return requests.get(url, headers=headers)
+
+    def fetch_languages(self, active=True):
+        response = self.make_api_request("languages")
 
         if response.status_code == 200:
             data = response.json()
             return data
 
+    def get_daily_word_counts(self):
+        response = self.make_api_request("languages")
+
+        if response.status_code == 200:
+            data = response.json()
+            active_languages = [language for language in data if language['knownWords'] > 0]
+            return [{'date': datetime.now().isoformat(), 'language': language['title'],
+                     'word-count': language['knownWords']} for language in active_languages]
+
 
 if __name__ == '__main__':
-    fetcher = LingQFetcher("MY KEY")
-    languages = fetcher.fetch_languages()
-    print(languages[0])
+    fetcher = LingQFetcher()
+    languages = fetcher.get_daily_word_counts()
+    print(languages)
