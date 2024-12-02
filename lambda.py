e0ba90adf162367e48f1812d7ec86b6d23fe4750
Jake Godsall <jake.edward.godsall@gmail.com>
2024-12-02 21:38:16 +0000

    added styling to main.py

--- lambda.py
+++ lambda.py
@@ -6,9 +6,6 @@ from src.integrations.lingq.fetcher import LingQFetcher
 from src.integrations.whoop.fetcher import WhoopFetcher
 from src.services.notion import NotionClient
 
-# Adjust the path for project imports
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(project_root)
 
 # Configuration
 notion_config_path = Path(__file__).resolve().parent / "src" / "config" / "notion.config.yml"
@@ -18,13 +15,14 @@ lingq_service = LingQFetcher()
 whoop_service = WhoopFetcher()
 notion_client = NotionClient(notion_config_path)
 
-# Tasks
+
 def sync_lingq():
     print(f"[{datetime.now()}] Running LingQ sync...")
     word_counts = lingq_service.get_daily_word_counts()
     for word_count in word_counts:
         notion_client.create_page("lingq", word_count)
     print(f"[{datetime.now()}] LingQ sync completed.")
+
 
 def sync_whoop():
     print(f"[{datetime.now()}] Running WHOOP sync...")
@@ -34,7 +32,7 @@ def sync_whoop():
         notion_client.create_page("whoop", workout)
     print(f"[{datetime.now()}] WHOOP sync completed.")
 
-# Lambda handler
+
 def lambda_handler(event, context):
     print(f"[{datetime.now()}] Starting the synchronization tasks...")
     sync_lingq()
--- main.py
+++ main.py
@@ -6,24 +6,23 @@ from src.integrations.lingq.fetcher import LingQFetcher
 from src.integrations.whoop.fetcher import WhoopFetcher
 from src.services.notion import NotionClient
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(project_root)
-
 # Configuration
 notion_config_path = Path(__file__).resolve().parent / "src" / "config" / "notion.config.yml"
 
 # Initialize services
 lingq_service = LingQFetcher()
 whoop_service = WhoopFetcher()
-notion_client = NotionClient(notion_config_path)
+notion_client = NotionClient(str(notion_config_path))
 
-# Tasks
+
+
 def sync_lingq():
     print(f"[{datetime.now()}] Running LingQ sync...")
     word_counts = lingq_service.get_daily_word_counts()
     for word_count in word_counts:
         notion_client.create_page("lingq", word_count)
     print(f"[{datetime.now()}] LingQ sync completed.")
+
 
 def sync_whoop():
     print(f"[{datetime.now()}] Running WHOOP sync...")
@@ -33,6 +32,7 @@ def sync_whoop():
         notion_client.create_page("whoop", workout)
     print(f"[{datetime.now()}] WHOOP sync completed.")
 
-# Run both tasks
-sync_lingq()
-sync_whoop()
+
+if __name__ == "__main__":
+    sync_lingq()
+    sync_whoop()
