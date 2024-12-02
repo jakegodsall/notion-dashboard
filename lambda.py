30c0456342042318eb5b438ee7020362e3117d44
Jake Godsall <jake.edward.godsall@gmail.com>
2024-12-02 21:28:29 +0000

    added lambda.py

---
+++ lambda.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from pathlib import Path
+from datetime import datetime
+from src.integrations.lingq.fetcher import LingQFetcher
+from src.integrations.whoop.fetcher import WhoopFetcher
+from src.services.notion import NotionClient
+
+# Adjust the path for project imports
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
+
+# Configuration
+notion_config_path = Path(__file__).resolve().parent / "src" / "config" / "notion.config.yml"
+
+# Initialize services
+lingq_service = LingQFetcher()
+whoop_service = WhoopFetcher()
+notion_client = NotionClient(notion_config_path)
+
+# Tasks
+def sync_lingq():
+    print(f"[{datetime.now()}] Running LingQ sync...")
+    word_counts = lingq_service.get_daily_word_counts()
+    for word_count in word_counts:
+        notion_client.create_page("lingq", word_count)
+    print(f"[{datetime.now()}] LingQ sync completed.")
+
+def sync_whoop():
+    print(f"[{datetime.now()}] Running WHOOP sync...")
+    workouts = whoop_service.get_workouts_for_given_date(datetime.now().isoformat())
+    transformed_workouts = whoop_service.transform_workouts(workouts)
+    for workout in transformed_workouts:
+        notion_client.create_page("whoop", workout)
+    print(f"[{datetime.now()}] WHOOP sync completed.")
+
+# Lambda handler
+def lambda_handler(event, context):
+    print(f"[{datetime.now()}] Starting the synchronization tasks...")
+    sync_lingq()
+    sync_whoop()
+    print(f"[{datetime.now()}] All tasks completed.")
+    return {"status": "success"}
